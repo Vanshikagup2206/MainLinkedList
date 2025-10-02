8bc75d111997201c3a7567e0071085cddd8ebdc2
Vanshika Gupta <vanshikagupta0009@gmail.com>
2025-10-02 15:54:29 +0530

    commit

--- ReverseLinkedList.java
+++ ReverseLinkedList.java
@@ -1,15 +1,11 @@
-package MainLinkedList;
+class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 public class ReverseLinkedList {
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
